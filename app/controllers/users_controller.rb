7231bea97c845c95736eff48f325f98ffe18c886
Edward Treece <edtreece@gmail.com>
2022-09-07 10:25:44 +0100

    User controller done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+    if user_signed_in?
+      @user = current_user
+      @my_tickets = Ticket.where(owner: current_user)
+    else
+      redirect_to new_user_session_path
+    end
+  end
+end

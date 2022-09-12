b9b40636098b6528c1df04da526666baf3983ab7
Edward Treece <edtreece@gmail.com>
2022-09-12 16:09:07 +0100

    Ticket logic corrected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     if user_signed_in?
       @user = current_user
-      @my_tickets_selling = Ticket.where(owner: current_user)
+      @my_tickets_selling = Ticket.where(owner: current_user).and(Ticket.where(for_sale: true))
     else
       redirect_to new_user_session_path
     end

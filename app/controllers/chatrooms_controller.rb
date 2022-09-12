ec98671d20f30c7901e154b2a2c09ab2caa39010
Andrei Badea <andreinbadea7@gmail.com>
2022-09-12 23:48:19 +0100

    messages index page

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,8 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = current_user.chats
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new

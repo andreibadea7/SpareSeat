ddf1e2ab9c17f87b4cf3118fd755bad2d8cfbc4f
JamesGoforth <jamesngoforth@gmail.com>
2022-09-13 15:44:28 +0100

    table on events show

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,13 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import $ from "jquery"
 
 export default class extends Controller {
-  connect() {
-      $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-      });
+    function(){
+      $('#keywords').tablesorter();
     }
   }

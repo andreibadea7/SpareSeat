9a5c935bf4973a0f5aef1c82b6e907387d7748c8
Andrei Badea <andreinbadea7@gmail.com>
2022-09-15 00:36:56 +0100

    style changes to the messenger

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -4,7 +4,13 @@ class Chatroom < ApplicationRecord
   belongs_to :participant_one, class_name: "User"
   belongs_to :participant_two, class_name: "User"
 
+  # Returns the participants of a certain chatroom
   def participants
     User.where("id = ? OR id = ?", participant_one_id, participant_two_id)
+  end
+
+  # Returns the interlocutor of current user in a particular chatroom
+  def interlocutor_of(current_user)
+    participants.where.not("id = ?", current_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,24 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :orders
 
+  # Returns all chats
   def chats
     Chatroom.where("participant_one_id = ? OR participant_two_id = ?", id, id)
   end
 
+  # Returns all the chats with a certain user
   def chats_with(interlocutor)
     chats.where("participant_one_id = ? OR participant_two_id = ?", interlocutor, interlocutor)
   end
 
+  # Returns all transactions
   def transactions
     Order.where("seller_id = ? OR buyer_id = ?", id, id)
+  end
+
+  # Return full_name of user
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
   # Devise
